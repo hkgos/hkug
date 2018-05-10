b38bc84c6b1bd69d34e966a938fa6342ae3cfbe2
CK Wun <ckwun@connect.ust.hk>
2018-05-10 22:48:37 +0800

    remove text in homepage

--- hkug-frontend-react/src/containers/Home/index.jsx
+++ hkug-frontend-react/src/containers/Home/index.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { compose, pure } from 'recompose';
 import injectSheet from 'react-jss';
 
 const styles = {
 };
 
-const Home = ({ classes }) => (
-  <div>HOME</div>
+const Home = () => (
+  <div />
 );
-Home.propTypes = {
-  classes: PropTypes.shape({}).isRequired,
-};
 
 const enhance = compose(
   injectSheet(styles),
