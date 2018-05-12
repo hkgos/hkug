f7705d421feabe9b7e76803ec1a907cfc0487c10
CK Wun <ckwun@connect.ust.hk>
2018-05-13 03:30:57 +0800

    Add some content in home

--- hkug-frontend-react/src/containers/Home/index.jsx
+++ hkug-frontend-react/src/containers/Home/index.jsx
@@ -1,13 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { compose, pure } from 'recompose';
 import injectSheet from 'react-jss';
 
 const styles = {
+  container: {
+    flex: 'auto',
+    margin: '16px 16px 0',
+  },
 };
 
-const Home = () => (
-  <div />
+const GitHubLink = () => (
+  <a
+    target="_blank"
+    rel="noopener noreferrer"
+    href="https://github.com/hkgos/hkug"
+  >
+    GitHub
+  </a>
 );
+
+const Home = ({ classes }) => (
+  <div className={classes.container}>
+    <section>
+      <h1>HKUG 香港聯登</h1>
+      <p>高登 ＋ 連登 ＝ 聯登</p>
+    </section>
+    <section>
+      <h1>目標</h1>
+      <p>建立一個可以同時觀看高登及連登的討論區，方便兩登都想留意的用家。</p>
+    </section>
+    <section>
+      <h1>問題回報</h1>
+      <p>這是一個開源項目，有問題請到 <GitHubLink /> 回報。
+      </p>
+    </section>
+  </div>
+);
+
+Home.propTypes = {
+  classes: PropTypes.shape({}).isRequired,
+};
 
 const enhance = compose(
   injectSheet(styles),
