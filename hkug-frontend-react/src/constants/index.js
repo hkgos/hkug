d192505b178d12eb67bb0236e83a883f5b9df4ad
CK Wun <ckwun@connect.ust.hk>
2018-05-17 15:12:41 +0800

    fixup! extract SIDE_MENU_BREAK_POINT to constants

--- hkug-frontend-react/src/constants/index.js
+++ hkug-frontend-react/src/constants/index.js
@@ -4,3 +4,4 @@ export const LIHKG_API_ENDPOINT = 'https://lihkg.com/api_v2/';
 export const HKG_VIEW_TOPIC_BASE = 'https://web.hkgolden.com/view/';
 export const LIHKG_VIEW_TOPIC_BASE = 'https://lihkg.com/thread/';
 export const API_TIMEOUT = 10000;
+export const SIDE_MENU_BREAK_POINT = 992;
