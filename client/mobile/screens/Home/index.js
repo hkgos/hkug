61171e0672a856bf9d20180264d36649c2684e79
CK Wun <ckwun@connect.ust.hk>
2018-05-28 17:42:03 +0800

    Add Loading indicator

--- client/mobile/screens/Home/index.js
+++ client/mobile/screens/Home/index.js
@@ -1,37 +1,15 @@
 import React from 'react';
-import { StyleSheet, WebView, ActivityIndicator } from 'react-native';
+import { WebView } from 'react-native';
 import { Container } from 'native-base';
 import Header from '../Header';
-
-const styles = StyleSheet.create({
-  webView: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 1,
-  },
-  activityIndicator: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    top: 0,
-    bottom: 0,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import Loading from '../../components/Loading';
 
 const Home = ({ navigation }) => (
   <Container>
     <Header navigation={navigation} />
     <WebView
-      style={styles.webView}
       startInLoadingState
-      renderLoading={() => (
-        <ActivityIndicator
-          size="large"
-          style={styles.activityIndicator}
-        />
-      )}
+      renderLoading={Loading}
       source={{ uri: 'https://github.com/hkgos/hkug/blob/master/README.md' }}
     />
   </Container>
