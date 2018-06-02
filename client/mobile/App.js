4b174c26b30808db6d6cd23fa7477a5e4cb50b96
hk-ng <tommy123ng@gmail.com>
2018-06-03 03:35:57 +0800

    add font

--- client/mobile/App.js
+++ client/mobile/App.js
@@ -3,18 +3,22 @@ import 'moment/locale/zh-hk';
 import React from 'react';
 import { createDrawerNavigator } from 'react-navigation';
 import { Provider } from 'react-redux';
+import { Font } from 'expo';
 import moment from 'moment';
 import Drawer from './screens/Drawer';
 import Home from './screens/Home';
 import Topics from './screens/Topics';
 import Thread from './screens/Thread';
 import configureStore from './store/configureStore';
+import robotoMedium from './assets/fonts/Roboto-Medium.ttf';
+
 
 moment.locale('zh-hk');
 
 const store = configureStore();
 
-const App = createDrawerNavigator(
+
+const Navigator = createDrawerNavigator(
   {
     Home,
     Topics,
@@ -25,6 +29,23 @@ const App = createDrawerNavigator(
     contentComponent: props => <Drawer {...props} />,
   },
 );
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { fontLoaded: false };
+  }
+  componentDidMount() {
+    Font.loadAsync({
+      Roboto_medium: robotoMedium,
+    // eslint-disable-next-line react/no-did-mount-set-state
+    }).then(this.setState({ fontLoaded: true }));
+  }
+
+  render() {
+    return this.state.fontLoaded ? (<Navigator />) : null;
+  }
+}
 
 export default () => (
   <Provider store={store}>
